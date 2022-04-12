a6661969caba636b516d3c12e6bd133c49320d0b
alade93 <99254792+alade93@users.noreply.github.com>
2022-04-12 01:32:01 -0700

    Add files via upload

---
+++ object.js
@@ -0,0 +1,96 @@
+// const dog = {
+//   name: Bingo,
+//   legs: 4,
+//   color: Black,
+//   age: 8,
+//   bark: woof,
+//   woof,
+// };
+// console.log(dog);
+// const copydog = obecy.assign({}, dog);
+// console.log(copydog);
+// const keys = object.keys(copydog);
+// console.log(copydog);
+// dog.breed = rottweiler;
+// dog.getDogInfo = function () {
+//   let statement = `${name} is a ${this.breed}.\nit has${this.legs}.\nthe color is ${this.color}.\nit is ${this.age}.\nit ${this.bark}very loud.`;
+// };
+// console.log(dog);
+// console.log(dog.getDogInfo());
+
+//exercise 2
+const users = {
+  Alex: {
+    email: "alex@alex.com",
+    skills: ["HTML", "CSS", "JavaScript"],
+    age: 20,
+    isLoggedIn: false,
+    points: 30,
+  },
+  Asab: {
+    email: "asab@asab.com",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Redux",
+      "MongoDB",
+      "Express",
+      "React",
+      "Node",
+    ],
+    age: 25,
+    isLoggedIn: false,
+    points: 50,
+  },
+  Brook: {
+    email: "daniel@daniel.com",
+    skills: ["HTML", "CSS", "JavaScript", "React", "Redux"],
+    age: 30,
+    isLoggedIn: true,
+    points: 50,
+  },
+  Daniel: {
+    email: "daniel@alex.com",
+    skills: ["HTML", "CSS", "JavaScript", "Python"],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+  John: {
+    email: "john@john.com",
+    skills: ["HTML", "CSS", "JavaScript", "React", "Redux", "Node.js"],
+    age: 20,
+    isLoggedIn: true,
+    points: 50,
+  },
+  Thomas: {
+    email: "thomas@thomas.com",
+    skills: ["HTML", "CSS", "JavaScript", "React"],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+  Paul: {
+    email: "paul@paul.com",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "MongoDB",
+      "Express",
+      "React",
+      "Node",
+    ],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+};
+// console.log(users.Alex.skills.length);
+// console.log(users.Asab.skills.length);
+// console.log(users.Brook.skills.length);
+// console.log(users.Daniel.skills.length);
+// console.log(users.John.skills.length);
+// console.log(users.Thomas.skills.length);
+// console.log(users.Paul.skills.length);
